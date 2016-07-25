1f82fa508764be75c25db996af620a7818972f51
Markus Tulin <macky_b@hotmail.com>
2016-07-25 19:59:38 +0200

    Adding config.php and bootstrap.php

---
+++ src/bootstrap.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Exception handler.
+ *
+ */
+function myExceptionHandler($exception) {
+    echo "Uncaught exception: <p>" . $exception->getMessage() . "</p><pre>" . $exception->getTraceAsString(), "</pre>";
+}
+set_exception_handler('myExceptionHandler');
+
+
+/**
+ * Autoloader
+ *
+ */
+function Autoloader($class) {
+    $path = "../src/{$class}/{$class}.php";
+    if(is_file($path)) {
+        include($path);
+    } else {
+        throw new Exception("Classfile '{$class}' does not exists.");
+    }
+}
+spl_autoload_register('Autoloader');
+
+/**
+ * Print 
+ *
+ */
+function prePrint($content) {
+    echo "<pre>" . print_r($content, 1) . "</pre>";
+}
