890f5daf9a0da519950a215bbd3d0f8d750f307c
Markus Tulin <macky_b@hotmail.com>
2017-07-31 21:26:35 +0200

    Added config file in test folder

---
+++ test/config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+* Config
+*
+* Get all configuration details to be able to execute the test suite.
+*
+* PHP version 7
+*
+* @category Config
+* @package  Tournament Bet
+* @author   Markus Thulin <macky_b@hotmail.com>
+* @license  http://www.opensource.org/licenses/mit-license.php MIT
+* @link     https://github.com/thulin82/tournament_bet
+*/
+require __DIR__ . '/../autoloader.php';
