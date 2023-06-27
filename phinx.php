f62254d4e1143b960984992f030bdc1cab977409
Markus Thulin <macky_b@hotmail.com>
2023-06-27 13:33:26 +0200

    Formatted newly created phinx.php to avoid validation errors

--- phinx.php
+++ phinx.php
@@ -2,25 +2,25 @@
 
 return
 [
-    'paths' => [
-        'migrations' => './db/migrations',
-        'seeds' => './db/seeds'
-    ],
-    'environments' => [
-        'default_migration_table' => 'phinxlog',
-        'default_database' => 'development',
-        'production' => [
-            'adapter' => 'sqlite',
-            'name' => 'db/tournament.sqlite'
-        ],
-        'development' => [
-            'adapter' => 'sqlite',
-            'name' => 'db/tournament.sqlite'
-        ],
-        'testing' => [
-            'adapter' => 'sqlite',
-            'name' => 'db/tournament.sqlite'
-        ]
-    ],
-    'version_order' => 'creation'
+ 'paths'         => [
+                     'migrations' => './db/migrations',
+                     'seeds'      => './db/seeds',
+                    ],
+ 'environments'  => [
+                     'default_migration_table' => 'phinxlog',
+                     'default_database'        => 'development',
+                     'production'              => [
+                                                   'adapter' => 'sqlite',
+                                                   'name'    => 'db/tournament.sqlite',
+                                                  ],
+                     'development'             => [
+                                                   'adapter' => 'sqlite',
+                                                   'name'    => 'db/tournament.sqlite',
+                                                  ],
+                     'testing'                 => [
+                                                   'adapter' => 'sqlite',
+                                                   'name'    => 'db/tournament.sqlite',
+                                                  ],
+                    ],
+ 'version_order' => 'creation',
 ];
