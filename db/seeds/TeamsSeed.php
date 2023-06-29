dd1b32cbf1e92e4494a7fed8a77f304d76c91442
Markus Thulin <macky_b@hotmail.com>
2023-06-29 13:59:16 +0200

    Used real teams in Teams seed instead of fake ones

--- db/seeds/TeamsSeed.php
+++ db/seeds/TeamsSeed.php
@@ -6,13 +6,17 @@ class TeamsSeed extends AbstractSeed
 {
     public function run() : void
     {
-        $faker = Faker\Factory::create();
         $data = [];
-        for ($i = 0; $i < 10; $i++) {
-            $data[] = [
-                'name' => $faker->country,
-            ];
-        }
+        $data[] = ['name' => 'Argentina'];
+        $data[] = ['name' => 'Brazil'];
+        $data[] = ['name' => 'Germany'];
+        $data[] = ['name' => 'Italy'];
+        $data[] = ['name' => 'France'];
+        $data[] = ['name' => 'Spain'];
+        $data[] = ['name' => 'England'];
+        $data[] = ['name' => 'Portugal'];
+        $data[] = ['name' => 'Netherlands'];
+        $data[] = ['name' => 'Belgium'];
         $this->insert('Teams', $data);
     }
 }
