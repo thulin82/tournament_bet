e8f9b431eb9250e648d9752a28dfa4c776b4a2a1
Markus Thulin <macky_b@hotmail.com>
2023-06-29 08:18:23 +0200

    Added faker data to seed Users table

--- db/seeds/UsersSeed.php
+++ db/seeds/UsersSeed.php
@@ -6,12 +6,13 @@ class UsersSeed extends AbstractSeed
 {
     public function run() : void
     {
+        $faker = Faker\Factory::create();
         $data = [];
         for ($i = 0; $i < 10; $i++) {
             $data[] = [
-                'name'      => 'userName' . $i,
-                'email'      => 'userMail' . $i . '@mail.com',
-                'password'    => 'userSecret' . $i,
+                'name'      => $faker->name,
+                'email'      => $faker->email,
+                'password'    => 'password' . $i,
             ];
         }
         $this->insert('Users', $data);
