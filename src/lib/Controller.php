c78f23929e4e1f2b8de48bccc13f8fa7cd682644
Markus Thulin <macky_b@hotmail.com>
2023-07-03 16:06:35 +0200

    Fixed some phpcs errors

--- src/lib/Controller.php
+++ src/lib/Controller.php
@@ -1,16 +1,19 @@
 <?php
-    class Controller {
+class Controller
+{
 
-        public function model($model){
-            require_once '../src/models/' . $model . '.php';
-            return new $model();
-        }
+    public function model($model)
+    {
+        include_once '../src/models/' . $model . '.php';
+        return new $model();
+    }
 
-        public function view($view, $data = []) {
-            if(file_exists('../src/views/' . $view . '.php')){
-                require_once '../src/views/' . $view . '.php';
-            } else {
-                die('View does not exist');
-            }
+    public function view($view, $data = [])
+    {
+        if (file_exists('../src/views/' . $view . '.php')) {
+            include_once '../src/views/' . $view . '.php';
+        } else {
+            die('View does not exist');
         }
     }
+}
