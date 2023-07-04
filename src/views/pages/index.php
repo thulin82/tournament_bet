db5b4ee036b71765f763a04fa1332169c8891a02
Markus Thulin <macky_b@hotmail.com>
2023-07-04 22:28:56 +0200

    Added inc/footer and included it

--- src/views/pages/index.php
+++ src/views/pages/index.php
@@ -1,6 +1,7 @@
-<div class="p-5 mb-4 bg-light rounded-3 text-center">
-    <div class="container-fluid py-5">
-        <h1 class="display-3"><?php echo $data['title'] ?></h1>
-        <p class="lead"><?php echo $data['description'] ?></p>
+    <div class="p-5 mb-4 bg-light rounded-3 text-center">
+        <div class="container-fluid py-5">
+            <h1 class="display-3"><?php echo $data['title'] ?></h1>
+            <p class="lead"><?php echo $data['description'] ?></p>
+        </div>
     </div>
-</div>
+<?php require APPROOT . '/views/inc/footer.php';
