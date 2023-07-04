8468eb62a98ad3bbb5be3944bb63a1cf53ff815c
Markus Thulin <macky_b@hotmail.com>
2023-07-04 22:37:18 +0200

    Added inc/header and included it

---
+++ src/views/inc/header.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title><?php echo APPNAME; ?></title>
+</head>
+<body>
+
+<div class="container">
--- src/views/pages/about.php
+++ src/views/pages/about.php
@@ -1,3 +1,4 @@
+<?php require APPROOT . '/views/inc/header.php';?>
     <div class="p-5 mb-4 bg-light rounded-3 text-center">
         <div class="container-fluid py-5">
             <h1 class="display-3"><?php echo $data['title'] ?></h1>
