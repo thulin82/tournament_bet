73a2978e3988129113a96bf9ca018af3d36982de
Markus Thulin <macky_b@hotmail.com>
2023-07-07 16:11:02 +0200

    Removed index_old.php and put sample data in controller/view instead(will move)

--- src/controllers/Pages.php
+++ src/controllers/Pages.php
@@ -17,9 +17,26 @@ class Pages extends Controller
      */
     public function index()
     {
+        $db_options['dsn']         = 'sqlite:../db/tournament.sqlite';
+        $db_options['username']    = '';
+        $db_options['password']    = '';
+        $db_options['fetch_style'] = PDO::FETCH_OBJ;
+
+        $db = new Database($db_options);
+        $db->connect();
+        $sql = 'SELECT m.id, t.name AS tournament_name, tm1.name AS team1_name, tm2.name AS team2_name, m.match_date
+                FROM matches m
+                INNER JOIN tournament t ON m.tournament_id = t.id
+                INNER JOIN teams tm1 ON m.team1_id = tm1.id
+                INNER JOIN teams tm2 ON m.team2_id = tm2.id
+                WHERE m.tournament_id = ?';
+        $param = array(1);
+        $res = $db->executeQueryFetchAll($sql, $param);
+
         $data = [
                  'title'       => 'Index',
                  'description' => 'Tournament Bet',
+                 'matches'     => $res,
                 ];
 
         $this->view('pages/index', $data);
