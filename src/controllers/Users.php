5461860661faf240979148febe0eb7d59f3c0b5a
Markus Thulin <macky_b@hotmail.com>
2023-07-21 22:21:09 +0200

    Added logout method

--- src/controllers/Users.php
+++ src/controllers/Users.php
@@ -17,4 +17,17 @@ class Users extends Controller
     {
         $this->userModel = $this->model('User');
     }
+
+    /**
+     * Logout user
+     *
+     * @return void
+     */
+    public function logout() {
+        unset($_SESSION['user_id']);
+        unset($_SESSION['user_email']);
+        unset($_SESSION['user_name']);
+        session_destroy();
+        redirect('');
+    }
 }
