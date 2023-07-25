6b96f4ecf2a7d099a46a9c4da6699f75865f09e3
Markus Thulin <macky_b@hotmail.com>
2023-07-25 22:30:20 +0200

    Changed seeding of user data to include admin account

--- db/seeds/UsersSeed.php
+++ db/seeds/UsersSeed.php
@@ -8,11 +8,16 @@ class UsersSeed extends AbstractSeed
     {
         $faker = Faker\Factory::create();
         $data = [];
-        for ($i = 1; $i < 11; $i++) {
+        $data[] = [
+            'name'      => 'admin',
+            'email'      => 'admin@admin.com',
+            'password'    => password_hash('admin', PASSWORD_DEFAULT),
+        ];
+        for ($i = 1; $i < 10; $i++) {
             $data[] = [
                 'name'      => $faker->name,
                 'email'      => $faker->email,
-                'password'    => 'password' . $i,
+                'password'    => password_hash('password' . $i, PASSWORD_DEFAULT),
             ];
         }
         $this->insert('Users', $data);
