046b5290282c53359f152382bf59b805695d55a0
Markus Thulin <macky_b@hotmail.com>
2023-07-28 08:11:20 +0200

    Included session helper in bootstrap code

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -3,6 +3,7 @@
 require_once 'config/config.php';
 require_once '../vendor/autoload.php';
 require_once 'helpers/url_helper.php';
+require_once 'helpers/session_helper.php';
 
 /**
  * Exception handler.
