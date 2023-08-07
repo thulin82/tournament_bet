41e1d6edd8e75c6efc45845b248ba7ae77f6a5b9
Markus Thulin <macky_b@hotmail.com>
2023-08-07 13:30:22 +0200

    Added custom css

--- src/views/inc/header.php
+++ src/views/inc/header.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
     <title><?php echo APPNAME; ?></title>
 </head>
 <body>
