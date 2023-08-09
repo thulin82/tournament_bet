d2a468c249004660c8bc696d482f0bf0903632d1
Markus Thulin <macky_b@hotmail.com>
2023-08-09 15:54:34 +0200

    Added basic favicon

--- src/views/inc/header.php
+++ src/views/inc/header.php
@@ -6,6 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
+    <link rel="icon" type="image/png" href="/img/favicon-512.png" sizes="512x512" />
     <title><?php echo APPNAME; ?></title>
 </head>
 <body>
