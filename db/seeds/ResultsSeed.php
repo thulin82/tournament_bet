3ea3b6b5dc6ad2fc64294743d28f65ad057e102b
Markus Thulin <macky_b@hotmail.com>
2023-08-09 15:33:17 +0200

    Seed some results

--- db/seeds/ResultsSeed.php
+++ db/seeds/ResultsSeed.php
@@ -6,5 +6,9 @@ class ResultsSeed extends AbstractSeed
 {
     public function run() : void
     {
+        $data = [];
+        $data[] = ['match_id' => 1, 'result' => '1'];
+        $data[] = ['match_id' => 2, 'result' => 'X'];
+        $this->insert('Results', $data);
     }
 }
