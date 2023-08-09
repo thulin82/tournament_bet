a2ba438b79a4ee8ba80dffd8298ae1b430006fc9
Markus Thulin <macky_b@hotmail.com>
2023-08-09 09:49:42 +0200

    Change to short variable printout

--- src/views/bets/index.php
+++ src/views/bets/index.php
@@ -1,42 +1,36 @@
 <?php require APPROOT . '/views/inc/header.php';?>
-    <h1 class="display-3"><?php echo $data['title'] ?></h1>
+    <h1 class="display-3"><?=$data['title']?></h1>
+    <div class="row g-3">
+        <div class="col-sm"><h3>Match</h3></div>
+        <div class="col-sm"><h3>Date</h3></div>
+        <div class="col-sm"><h3>Result</h3></div>
+    </div>
     <?php
-    echo
-    '<div class="row g-3">
-        <div class="col-sm">
-            <h3>Match</h3>
-        </div>
-        <div class="col-sm">
-            <h3>Date</h3>
-        </div>
-        <div class="col-sm">
-            <h3>Result</h3>
-        </div>
-    </div>';
     foreach ($data['matches'] as $row) {
-        echo
-        '<div class="row g-3">
+    ?>
+        <div class="row g-3">
             <div class="col-sm">
-            ' . $row->team1_name . ' - ' . $row->team2_name .'
+                <?=$row->team1_name?> - <?=$row->team2_name?>
             </div>
             <div class="col-sm">
-            ' . $row->match_date .'
+                <?=$row->match_date?>
             </div>
             <div class="col-sm">
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="inlineRadioOptions' . $row->id . '" id="radio1_' . $row->id .'" value="1">
-                    <label class="form-check-label" for="radio1_' . $row->id .'">1</label>
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions<?=$row->id?>" id="radio1_<?=$row->id?>" value="1">
+                    <label class="form-check-label" for="radio1_<?=$row->id?>">1</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="inlineRadioOptions' . $row->id . '" id="radioX_' . $row->id .'" value="X">
-                    <label class="form-check-label" for="radioX_' . $row->id .'">X</label>
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions<?=$row->id?>" id="radioX_<?=$row->id?>" value="X">
+                    <label class="form-check-label" for="radioX_<?=$row->id?>">X</label>
                 </div>
                 <div class="form-check form-check-inline">
-                    <input class="form-check-input" type="radio" name="inlineRadioOptions' . $row->id . '" id="radio2_' . $row->id .'" value="2">
-                    <label class="form-check-label" for="radio2_' . $row->id .'">2</label>
+                    <input class="form-check-input" type="radio" name="inlineRadioOptions<?=$row->id?>" id="radio2_<?=$row->id?>" value="2">
+                    <label class="form-check-label" for="radio2_<?=$row->id?>">2</label>
                 </div>
             </div>
-      </div>';
+      </div>
+    <?php
     }//end foreach
     ?>
 <?php require APPROOT . '/views/inc/footer.php';
