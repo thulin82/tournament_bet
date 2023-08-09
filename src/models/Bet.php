5d720cb14386d40a05af754f74731d54e222c5aa
Markus Thulin <macky_b@hotmail.com>
2023-08-09 11:31:54 +0200

    New model to only return matches where the user have placed bets

--- src/models/Bet.php
+++ src/models/Bet.php
@@ -44,6 +44,31 @@ class Bet
     }
 
     /**
+     * List matches by tournament and with bets by specific user
+     *
+     * @param int $userId       The user id
+     * @param int $tournamentId The tournament id
+     *
+     * @return array
+     */
+    public function listMatchesWithBetsByTournamentAndUser($userId, $tournamentId) : array
+    {
+        $this->db->query(
+            'SELECT m.id, t.name AS tournament_name, tm1.name AS team1_name, tm2.name AS team2_name, m.match_date, b.bet
+            FROM matches m
+            INNER JOIN tournament t ON m.tournament_id = t.id
+            INNER JOIN teams tm1 ON m.team1_id = tm1.id
+            INNER JOIN teams tm2 ON m.team2_id = tm2.id
+            INNER JOIN bets b ON m.id = b.match_id AND b.user_id = :userId
+            WHERE m.tournament_id = :tournamentId'
+        );
+        $this->db->bind(':userId', $userId);
+        $this->db->bind(':tournamentId', $tournamentId);
+        $res = $this->db->resultSet();
+        return $res;
+    }
+
+    /**
      * Get name of tournament
      *
      * @param int $id The tournament id
