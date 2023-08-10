9fe0f63880d9527447c9e6ab67c86a311fb489b0
Markus Thulin <macky_b@hotmail.com>
2023-08-10 15:15:48 +0200

    Change font awesome to bootstrap icons and made use of them

--- src/views/inc/header.php
+++ src/views/inc/header.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/style.css">
     <link rel="icon" type="image/png" href="/img/favicon-512.png" sizes="512x512" />
     <title><?php echo APPNAME; ?></title>
--- src/views/inc/navbar.php
+++ src/views/inc/navbar.php
@@ -8,17 +8,17 @@
     <div class="collapse navbar-collapse" id="navbarsExampleDefault">
       <ul class="navbar-nav me-auto">
         <li class="nav-item">
-          <a class="nav-link <?php if ($_SERVER['REQUEST_URI'] =='/') {echo 'active';} ?>" href="<?php echo URLROOT; ?>">Home</a>
+          <a class="nav-link <?php if ($_SERVER['REQUEST_URI'] =='/') {echo 'active';} ?>" href="<?php echo URLROOT; ?>"><i class="bi bi-house me-1"></i>Home</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link <?php if ($_SERVER['REQUEST_URI'] == '/pages/about') {echo 'active';} ?>" href="<?php echo URLROOT; ?>/pages/about">About</a>
+          <a class="nav-link <?php if ($_SERVER['REQUEST_URI'] == '/pages/about') {echo 'active';} ?>" href="<?php echo URLROOT; ?>/pages/about"><i class="bi bi-question-circle me-1"></i>About</a>
         </li>
         <?php if (isset($_SESSION['user_id'])) : ?>
         <li class="nav-item">
-          <a class="nav-link <?php if ($_SERVER['REQUEST_URI'] =='/bets') {echo 'active';} ?>" href="<?php echo URLROOT; ?>/bets">Bets</a>
+          <a class="nav-link <?php if ($_SERVER['REQUEST_URI'] =='/bets') {echo 'active';} ?>" href="<?php echo URLROOT; ?>/bets"><i class="bi bi-cash-coin me-1"></i>Bets</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link <?php if ($_SERVER['REQUEST_URI'] =='/bets/results') {echo 'active';} ?>" href="<?php echo URLROOT; ?>/bets/results">Results</a>
+          <a class="nav-link <?php if ($_SERVER['REQUEST_URI'] =='/bets/results') {echo 'active';} ?>" href="<?php echo URLROOT; ?>/bets/results"><i class="bi bi-trophy me-1"></i>Results</a>
         </li>
         <?php endif; ?>
       </ul>
@@ -28,14 +28,14 @@
           <a class="nav-link" href="#">Logged in as <?php echo $_SESSION['user_name']; ?></a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="<?php echo URLROOT; ?>/users/logout">Logout</a>
+          <a class="nav-link" href="<?php echo URLROOT; ?>/users/logout"><i class="bi bi-box-arrow-right me-1"></i>Logout</a>
         </li>
       <?php else : ?>
         <li class="nav-item">
-          <a class="nav-link <?php if ($_SERVER['REQUEST_URI'] =='/users/register') {echo 'active';} ?>" href="<?php echo URLROOT; ?>/users/register">Register</a>
+          <a class="nav-link <?php if ($_SERVER['REQUEST_URI'] =='/users/register') {echo 'active';} ?>" href="<?php echo URLROOT; ?>/users/register"><i class="bi bi-person-add me-1"></i>Register</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link <?php if ($_SERVER['REQUEST_URI'] =='/users/login') {echo 'active';} ?>" href="<?php echo URLROOT; ?>/users/login">Login</a>
+          <a class="nav-link <?php if ($_SERVER['REQUEST_URI'] =='/users/login') {echo 'active';} ?>" href="<?php echo URLROOT; ?>/users/login"><i class="bi bi-box-arrow-in-right me-1"></i>Login</a>
         </li>
       <?php endif; ?>
       </ul>
