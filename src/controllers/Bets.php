cd50aeb3f4afaf3293edca609898420e2fb12bb9
Markus Thulin <macky_b@hotmail.com>
2023-08-11 10:37:41 +0200

    Changes to index action in bets controller to store bets to db

--- src/controllers/Bets.php
+++ src/controllers/Bets.php
@@ -31,17 +31,35 @@ class Bets extends Controller
         if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $_POST = filter_input_array(INPUT_POST, FILTER_UNSAFE_RAW);
             // Proccess form.
-            print_r($_POST);
-        }
+            $data = [
+                     'title'   => '',
+                     'matches' => '',
+                     'err'     => '',
+                    ];
 
-        $tournament_name = $this->betModel->getTournamentName(1);
-        $tournament_games = $this->betModel->listMatchesWithNoBetsByTournamentAndUser(1, 1);
-        $data = [
-                 'title'   => $tournament_name,
-                 'matches' => $tournament_games,
-                ];
+            if (empty($_POST)) {
+                $data['err'] = 'Please fill in the form';
+            }
 
-        $this->view('bets/index', $data);
+            if (empty($data['err'])) {
+                // There are values we need to proccess.
+                if ($this->betModel->storeBets($_POST)) {
+                    redirect('bets/results');
+                } else {
+                    die('Something went wrong');
+                }
+            } else {
+                $data['err'] = 'Something went wrong';
+                $this->view('bets/index', $data);
+            }
+        } else {
+            $data = [
+                     'title'   => $this->betModel->getTournamentName(1),
+                     'matches' => $this->betModel->listMatchesWithNoBetsByTournamentAndUser(1, 1),
+                     'err'     => '',
+                    ];
+            $this->view('bets/index', $data);
+        }//end if
     }
 
     /**
