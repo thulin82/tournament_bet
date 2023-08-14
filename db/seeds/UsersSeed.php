5a379b61ae014920282bd532aeb5fa91c49bdf0e
Markus Thulin <macky_b@hotmail.com>
2023-08-14 16:33:21 +0200

    Add seeds for Groups, Roles and updated Users

--- db/seeds/UsersSeed.php
+++ db/seeds/UsersSeed.php
@@ -12,12 +12,17 @@ class UsersSeed extends AbstractSeed
             'name'      => 'admin',
             'email'      => 'admin@admin.com',
             'password'    => password_hash('admin', PASSWORD_DEFAULT),
+            'role_id'      => 1,
+            'group_id'      => 1,
+
         ];
         for ($i = 1; $i < 10; $i++) {
             $data[] = [
                 'name'      => $faker->name,
                 'email'      => $faker->email,
                 'password'    => password_hash('password' . $i, PASSWORD_DEFAULT),
+                'role_id'      => 2,
+                'group_id'      => 1,
             ];
         }
         $this->insert('Users', $data);
