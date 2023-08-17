c0e8694368ccf4886dbfe8e356d45d324a4d6f54
Markus Thulin <macky_b@hotmail.com>
2023-08-17 13:14:05 +0200

    Add migration and seed for points

--- db/seeds/BetsSeed.php
+++ db/seeds/BetsSeed.php
@@ -7,7 +7,7 @@ class BetsSeed extends AbstractSeed
     public function run() : void
     {
         $data = [];
-        $data[] = ['user_id' => 1, 'match_id' => 1, 'bet' => '1'];
+        $data[] = ['user_id' => 1, 'match_id' => 1, 'bet' => '1', 'points' => 1];
         $data[] = ['user_id' => 2, 'match_id' => 1, 'bet' => 'X'];
         $data[] = ['user_id' => 3, 'match_id' => 1, 'bet' => '2'];
         $data[] = ['user_id' => 1, 'match_id' => 2, 'bet' => '1'];
