fbf3962ca51d6b7043460e7254724511052bbb6a
Markus Thulin <macky_b@hotmail.com>
2023-08-28 20:49:09 +0200

    Displayed a list of users in admin/index view

--- src/views/admins/index.php
+++ src/views/admins/index.php
@@ -1,7 +1,26 @@
 <?php require APPROOT . '/views/inc/header.php';?>
-    <div class="p-5 mb-4 bg-light rounded-3 text-center">
-        <div class="container-fluid py-5">
-            <h1 class="display-3"><?php echo $data['title'] ?></h1>
+    <h1 class="display-3">Admin view for <?php echo $data['group_name']->name ?></h1>
+    <div class="row g-3">
+        <div class="col-sm"><h3>Id</h3></div>
+        <div class="col-sm"><h3>Name</h3></div>
+        <div class="col-sm"><h3>Total Points</h3></div>
+    </div>
+    <?php
+    foreach ($data['group_members'] as $row) {
+        ?>
+    <div class="row g-3">
+        <div class="col-sm">
+            <?=$row->user_id?>
+        </div>
+        <div class="col-sm">
+            <?=$row->user_name?>
+        </div>
+        <div class="col-sm">
+            <?=$row->total_points?>
         </div>
     </div>
+        <?php
+    }//end foreach
+    ?>
+
 <?php require APPROOT . '/views/inc/footer.php';
